eee4be1ec13cfa6abf9d3dea88a92bd108247e8d
Hilal ABACI <hilalabaci55@gmail.com>
2025-03-07 21:14:27 +0000

    updated sprint api

--- src/controllers/sprintController.ts
+++ src/controllers/sprintController.ts
@@ -5,25 +5,34 @@ const prisma = new PrismaClient();
 
 export const addSprint = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { name, sprintGoal, startDate, endDate, boardId, userId } = req.body;
+    const { boardId, userId } = req.body;
 
-    const foundBoards = await prisma.board.findMany({
+    const board = await prisma.board.findFirst({
       where: {
         Id: boardId,
-        UserBoards: { some: { UserId: userId } },
+        UserBoards: { some: { UserId: userId, Role: "Admin" } },
       },
     });
-    if (!foundBoards.length) {
+    if (!board) {
       res.status(400).json({
-        message: "board not found",
+        message: "You do not have permission to add a sprint to this board",
       });
       return;
     }
+    const sprints = await prisma.sprint.findMany({
+      where: {
+        BoardId: boardId,
+      },
+    });
+    const sprintsLenght = sprints.length;
+    const startDate = new Date();
+    const endDate = new Date();
+    endDate.setDate(startDate.getDate() + 21);
 
     const newSprint = await prisma.sprint.create({
       data: {
-        Name: name,
-        SprintGoal: sprintGoal,
+        Name: `${board.Key} Sprint ${sprintsLenght + 1}`,
+        SprintGoal: "",
         StartDate: startDate,
         EndDate: endDate,
         BoardId: boardId,
@@ -97,31 +106,55 @@ export const updateSprint = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
-  const sprintId = req.body.sprintId as string | undefined;
-  const boardId = req.body.boardId;
+  const {
+    boardId,
+    sprintId,
+    sprintName,
+    sprintGoal,
+    startDate,
+    endDate,
+    userId,
+  } = req.body;
+  //might be 2 scenario
+  //1. Make Active Sprint to sprintId
+  //2. Change data on sprint
   try {
-    // Update the selected sprint to active: true
+    if (sprintName || sprintGoal || startDate || endDate) {
+      const updatedSprint = await prisma.sprint.update({
+        where: { Id: sprintId },
+        data: {
+          Name: sprintName,
+          SprintGoal: sprintGoal,
+          StartDate: startDate,
+          EndDate: endDate,
+        },
+        include: { Issues: true },
+      });
+      res.status(200).json({
+        message: "Sprint updated successfully",
+        updatedSprint,
+      });
+    } else {
+      const updatedSprint = await prisma.sprint.update({
+        where: { Id: sprintId },
+        data: { IsActive: true },
+        include: { Issues: true },
+      });
 
-    const updatedSprint = await prisma.sprint.update({
-      where: { Id: sprintId },
-      data: { IsActive: true },
-      include: { Issues: true },
-    });
-
-    // Set active: false for all other sprints
-    const updateInactiveSprints = await prisma.sprint.updateMany({
-      where: {
-        Id: { not: sprintId },
-        BoardId: boardId,
-      },
-      data: { IsActive: false },
-    });
-
-    res.status(200).json({
-      message: "Sprint updated successfully",
-      updatedSprint,
-      updateInactiveSprints,
-    });
+      // Set active: false for all other sprints
+      const updateInactiveSprints = await prisma.sprint.updateMany({
+        where: {
+          Id: { not: sprintId },
+          BoardId: boardId,
+        },
+        data: { IsActive: false },
+      });
+      res.status(200).json({
+        message: "Sprint updated successfully",
+        updatedSprint,
+        updateInactiveSprints,
+      });
+    }
   } catch (error) {
     res.status(500).json({ message: "An error occurred", error });
   }
